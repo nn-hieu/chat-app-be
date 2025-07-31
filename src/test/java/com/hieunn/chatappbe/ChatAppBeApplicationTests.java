250ed963cf098c31b4248aefda5cbeb4f8f3cce9
nn-hieu <nn.hieu0701@gmail.com>
2025-07-31 19:48:01 +0700

    Init

---
+++ src/test/java/com/hieunn/chatappbe/ChatAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hieunn.chatappbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
