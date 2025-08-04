d631f2f943648c0c4e025ef02f6e54542e069ec9
nn-hieu <nn.hieu0701@gmail.com>
2025-08-04 12:49:11 +0700

    Jwt util

---
+++ src/main/java/com/hieunn/chatappbe/utils/JwtUtil.java
@@ -0,0 +1,93 @@
+package com.hieunn.chatappbe.utils;
+
+import com.hieunn.chatappbe.dtos.responses.UserDTO;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.function.Function;
+
+@Component
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class JwtUtil {
+    @Value("${security.jwt.secret-key}")
+    String secretKey;
+
+    @Value("${security.jwt.expiration}")
+    long jwtExpiration;
+
+    Key signingKey;
+
+    @PostConstruct
+    public void init() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        signingKey = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UserDTO userDto) {
+        return generateToken(new HashMap<>(), userDto);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDTO userDto) {
+        return buildToken(extraClaims, userDto, jwtExpiration);
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            extractAllClaims(token);
+            return true;
+        } catch (Exception ignored) {
+            return false;
+        }
+    }
+
+    public String extractUserId(String token) {
+        String userId = extractClaim(token, claims -> claims.get("userId", String.class));
+        if (userId != null) {
+            return userId;
+        }
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private String buildToken(
+            Map<String, Object> extraClaims,
+            UserDTO userDto,
+            long expiration
+    ) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDto.getUsername())
+                .setIssuer("hieunn")
+                .setId(UUID.randomUUID().toString())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .claim("userId", userDto.getId().toString())
+                .signWith(signingKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(signingKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
