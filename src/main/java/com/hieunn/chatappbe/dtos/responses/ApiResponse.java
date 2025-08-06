e8a90ffab8a48fe944297adec4edf9c718c07641
nn-hieu <nn.hieu0701@gmail.com>
2025-08-06 21:13:34 +0700

    Create api response

---
+++ src/main/java/com/hieunn/chatappbe/dtos/responses/ApiResponse.java
@@ -0,0 +1,43 @@
+package com.hieunn.chatappbe.dtos.responses;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+    int status;
+    T data;
+    String error;
+    String message;
+    LocalDateTime timestamp;
+
+    public static <T> ApiResponse<T> success(HttpStatus status, T data, String message) {
+        return ApiResponse.<T>builder()
+                .status(status.value())
+                .data(data)
+                .message(message)
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+
+    public static <T> ApiResponse<T> success(HttpStatus status, T data) {
+        return success(status, data, null);
+    }
+
+    public static <T> ApiResponse<T> error(HttpStatus status, String message) {
+        return ApiResponse.<T>builder()
+                .status(status.value())
+                .error(status.getReasonPhrase())
+                .message(message)
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+}
