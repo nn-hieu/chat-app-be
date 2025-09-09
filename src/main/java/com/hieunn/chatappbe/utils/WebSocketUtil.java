963c76c9202e88e0b3038f8b947fb212d18d3d5c
nn-hieu <nn.hieu0701@gmail.com>
2025-09-09 09:40:04 +0700

    WebSocket util

---
+++ src/main/java/com/hieunn/chatappbe/utils/WebSocketUtil.java
@@ -0,0 +1,27 @@
+package com.hieunn.chatappbe.utils;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+
+@Component
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+public class WebSocketUtil {
+    SimpMessagingTemplate messagingTemplate;
+
+    public void notifyUsers(List<Long> userIds, String destination, Object payload) {
+        for (Long userId : userIds) {
+            messagingTemplate.convertAndSendToUser(
+                    userId.toString(),
+                    destination,
+                    payload
+            );
+        }
+    }
+}
