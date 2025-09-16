91b174222669d6919f9cc51e3dd280d9c683e50a
nn-hieu <nn.hieu0701@gmail.com>
2025-09-16 19:38:28 +0700

    Add condition for property

--- src/main/java/com/hieunn/chatappbe/dtos/requests/TypingRequest.java
+++ src/main/java/com/hieunn/chatappbe/dtos/requests/TypingRequest.java
@@ -1,5 +1,6 @@
 package com.hieunn.chatappbe.dtos.requests;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,6 +12,9 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class TypingRequest {
+    @NotNull(message = "Conversation ID is required")
     Long conversationId;
+
+    @NotNull(message = "Typing status is required")
     Boolean isTyping;
 }
